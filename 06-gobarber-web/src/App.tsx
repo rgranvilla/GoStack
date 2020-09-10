e69d97b44f18864c030d0b021f9d19e262bb781d
Ricardo Granvilla <rgranvilla@gmail.com>
2020-09-10 01:05:45 -0300

    Estrutura das páginas GoBarber

---
+++ 06-gobarber-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import GlogalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlogalStyle />
+  </>
+);
+
+export default App;
